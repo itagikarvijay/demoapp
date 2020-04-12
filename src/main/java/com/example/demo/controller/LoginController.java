601dbc20fd618b8a3eca3744325c7baea4d2f3a4
itagikarvijay <36735973+itagikarvijay@users.noreply.github.com>
2020-04-12 12:10:41 +0530

    Update LoginController.java

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -10,7 +10,7 @@ public class LoginController {
 	@PreAuthorize("hasAnyRole('USER')")
 	@RequestMapping("/welcome")
 	public String getLoginMessage() {
-		System.out.println("WelcomeWelcomeddf");
+		System.out.println("Welcome");
 		return "welcome";
 	}
 
