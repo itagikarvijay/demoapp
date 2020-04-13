eb7a13ec04d0408982ffded4ad667afee0710018
vijay itagikar <itagikarv@gmail.com>
2020-04-13 15:51:47 +0530

    commit

---
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -0,0 +1,68 @@
+package com.example.demo.controller;
+
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	
+	@PreAuthorize("hasAnyRole('USER')")
+	@RequestMapping("/welcome")
+
+	public String getLoginMessage(Principal principal,Authentication authentication,HttpServletRequest request) {
+		
+		SecurityContext sc = SecurityContextHolder.getContext();
+		sc.setAuthentication(authentication);
+		
+		if(authentication.isAuthenticated()) {
+
+			HttpSession session = request.getSession(true);
+			session.setAttribute("USER_SESSION", authentication);
+		}
+		
+		
+		System.out.println(principal.getName());
+
+
+		return "welcome";
+	}
+
+	
+	@RequestMapping("/login")
+	public String login(String error) {
+	
+		return "login";
+	}
+
+	@RequestMapping("/public")
+	public String getPublicMessage() {
+		return "Public message";
+	}
+
+	@PreAuthorize("hasAnyRole('USER')")
+	@RequestMapping("/private")
+	public String getPrivateMessage() {
+		return "Private message";
+	}
+
+	@PreAuthorize("hasAnyRole('USER')")
+	@PostMapping("/invalidate")
+	public String logout(HttpServletRequest request) {
+		System.out.println("Logout.!");
+		SecurityContextHolder.getContext().setAuthentication(null);
+		
+		request.getSession().invalidate();
+		request.getSession();
+		return "redirect:/welcome";
+	}
+}
